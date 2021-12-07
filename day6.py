cea8c4333e68d6dcb30fce0fb46ff796855b71d2
Brian Ciccolo <brian@passingfocus.net>
2021-12-07 10:12:19 -0500

    Solved part 2

--- day6.py
+++ day6.py
@@ -1,5 +1,5 @@
 FILE_NAME = 'day6.dat'
-MAX_DAYS = 256
+MAX_DAYS = 80
 
 timers_by_day = []
 
@@ -21,7 +21,35 @@ def part1():
 
 
 def part2():
-    print('Part 2: ' + str(0))
+    # Part 2 seems simple: just change MAX_DAYS to 256. Except it won't ever finish - the
+    # simulate() algorithm grows exponentially! A new algorithm is needed instead. Rather
+    # than tracking each fish independently, we'll group them into age "buckets"
+    counts_by_age = [ 0, 0, 0, 0, 0, 0, 0, 0, 0 ] # Ages 0-8 days
+    file = open(FILE_NAME, 'r')
+    for fish in file.readlines()[0].strip().split(','):
+        age = int(fish)
+        counts_by_age[age] = counts_by_age[age] + 1
+
+    # print(counts_by_age)
+
+    for i in range(256):
+        # Rules:
+        # 1. Shift every age bucket one spot to the left
+        # 2. Bucket 0 goes to buckets 8 AND 6
+        age0 = counts_by_age[0]
+        for i in range(8):
+            counts_by_age[i] = counts_by_age[i + 1]
+
+        counts_by_age[6] = counts_by_age[6] + age0
+        counts_by_age[8] = age0
+
+        # print(counts_by_age)
+
+    sum = 0
+    for count in counts_by_age:
+        sum = sum + count
+
+    print('Part 2: ' + str(sum))
 
 
 def print_daily_timers():
@@ -33,7 +61,6 @@ def simulate():
     global timers_by_day
 
     for i in range(MAX_DAYS):
-        print('day ' + str(i) + '...')
         next_day = []
         today = timers_by_day[len(timers_by_day) - 1]
         new = 0
