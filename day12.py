60106df081f0862a2ab9bf38875c17460682efbd
Brian Ciccolo <brian@passingfocus.net>
2021-12-13 20:53:02 -0500

    Solved part 2 (and simplified part 1)

--- day12.py
+++ day12.py
@@ -1,30 +1,30 @@
-FILE_NAME = 'day12.dat'
+FILE_NAME = 'day12.dat' # Part 1 results: 10, 19, 226, 3713
 
 graph = {}
+small_caves = set()
 
 
-def count_paths(start, visited, this_path):
+def find_paths(start, visited, this_path, revisit, all_paths):
     connections = graph.get(start)
-
-    paths = 0
 
     for node in connections:
         if node == 'end':
             this_path.append(node)
-            # print(this_path)
-            paths = paths + 1
-        elif node != node.lower() or node not in visited:
+            all_paths.add(",".join(this_path))
+        elif node == node.upper() or node not in visited or node == revisit:
             this_path_copy = this_path.copy()
             this_path_copy.append(node)
             visited_copy = visited.copy()
-            visited_copy.add(node)
-            paths = paths + count_paths(node, visited_copy, this_path_copy)
 
-    return paths
+            if node == revisit:
+                find_paths(node, visited_copy, this_path_copy, None, all_paths)
+            else:
+                visited_copy.add(node)
+                find_paths(node, visited_copy, this_path_copy, revisit, all_paths)
 
 
 def load_data():
-    global graph
+    global graph, small_caves
 
     graph.clear()
 
@@ -50,24 +50,46 @@ def load_data():
             connections = [ endpoints[0] ]
             graph[endpoints[1]] = connections
 
+        # Remember the small caves (used in part 2)
+        if endpoints[0] == endpoints[0].lower():
+            small_caves.add(endpoints[0])
+
+        if endpoints[1] == endpoints[1].lower():
+            small_caves.add(endpoints[1])
+
+    # Remove the start/end caves from the set
+    small_caves.remove('start')
+    small_caves.remove('end')
+
+    # print(small_caves)
     # print(graph)
 
 
 def part1():
-    global graph
-
-    load_data()
-
+    all_paths = set()
     visited = set()
     visited.add('start')
-    paths = count_paths('start', visited, ['start'])
+    find_paths('start', visited, ['start'], None, all_paths)
 
-    print('Part 1: ' + str(paths))
+    # for path in all_paths:
+    #     print(path)
+
+    print('Part 1: ' + str(len(all_paths)))
 
 
 def part2():
-    print('Part 2: ' + str(0))
+    all_paths = set()
+    for small_cave in small_caves:
+        visited = set()
+        visited.add('start')
+        find_paths('start', visited, ['start'], small_cave, all_paths)
+
+    # for path in all_paths:
+    #     print(path)
+
+    print('Part 2: ' + str(len(all_paths)))
 
 
+load_data()
 part1()
 part2()
