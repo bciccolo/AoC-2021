9ac4e12f2bd111bc21be90426e4fa1d137e8b6a5
Brian Ciccolo <brian@passingfocus.net>
2021-12-14 21:22:42 -0500

    Solved part 2

--- day14.py
+++ day14.py
@@ -1,4 +1,4 @@
-FILE_NAME = 'day14.dat'
+FILE_NAME = 'day14-small.dat'
 
 polymer_string = ''
 rules = {}
@@ -15,6 +15,7 @@ def analyze_elements():
 
     return counts
 
+
 def load_data():
     global polymer_string, rules
 
@@ -23,6 +24,7 @@ def load_data():
 
     polymer_string = lines[0].strip()
 
+    rules.clear()
     for i in range(2, len(lines)):
         parts = lines[i].strip().split(' -> ')
         rules[parts[0]] = parts[1]
@@ -49,6 +51,42 @@ def grow_polymer(steps):
 
         # print('Step ' + str(i + 1) + ': ' + polymer_string)
 
+def grow_polymer_improved(steps):
+    counts = {}
+
+    # Step 1: Seed the counts with the original polymer string
+    for i in range(len(polymer_string) - 1):
+        element = polymer_string[i:i + 2]
+        if element not in counts:
+            counts[element] = 1
+        else:
+            counts[element] = counts[element] + 1
+
+    # Step 2: Update the counts for each step of the process (we don't care about order, only quantities)
+    for i in range(steps):
+        next_generation = {}
+        for element in counts:
+            current_count = counts[element]
+            insert = rules.get(element)
+
+            new_element_1 = element[0] + insert
+            if new_element_1 not in next_generation:
+                next_generation[new_element_1] = current_count
+            else:
+                next_generation[new_element_1] = next_generation[new_element_1] + current_count
+
+            new_element_2 = insert + element[1]
+            if new_element_2 not in next_generation:
+                next_generation[new_element_2] = current_count
+            else:
+                next_generation[new_element_2] = next_generation[new_element_2] + current_count
+
+        counts = next_generation
+
+    print(counts)
+
+    return counts
+
 
 def part1():
     load_data()
@@ -63,7 +101,35 @@ def part1():
 
 
 def part2():
-    print('Part 2: ' + str(0))
+    load_data()
+    counts_by_pair = grow_polymer_improved(40)
+
+    counts_by_element = {}
+    for pair in counts_by_pair:
+        count = counts_by_pair[pair]
+
+        element_1 = pair[0]
+        if element_1 not in counts_by_element:
+            counts_by_element[element_1] = count
+        else:
+            counts_by_element[element_1] = counts_by_element[element_1] + count
+
+        element_2 = pair[1]
+        if element_2 not in counts_by_element:
+            counts_by_element[element_2] = count
+        else:
+            counts_by_element[element_2] = counts_by_element[element_2] + count
+
+    print(counts_by_element)
+
+    sorted_counts = list(counts_by_element.values())
+    sorted_counts.sort()
+
+    # Not sure why this result ends in xxx.5; even less sure why the answer for the sample data
+    # is rounded UP but the answer for my full data set is rounded DOWN. I'll take it.
+    results = (sorted_counts[len(sorted_counts) - 1] - sorted_counts[0]) / 2
+
+    print('Part 2: ' + str(results))
 
 
 part1()
